d6b1289ef3fa310d0320e8a5bf6a09bc6405d0f5
antonp <antonp@il.ibm.com>
2021-01-20 09:21:03 +0200

    adding app starting code

--- app.py
+++ app.py
@@ -13,3 +13,6 @@ def hello():
 def show_user_profile(username):
     # show the user profile for that user
     return 'Hello %s' % username
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
